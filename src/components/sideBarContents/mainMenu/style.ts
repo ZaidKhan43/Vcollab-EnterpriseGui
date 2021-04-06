ece19e57003337c03f6705ead9c0bb75b3e56973
= <=>
2021-04-06 13:04:43 +0530

    fix formating error on mainmenu style

--- src/components/sideBarContents/mainMenu/style.ts
+++ src/components/sideBarContents/mainMenu/style.ts
@@ -1,9 +1,4 @@
 import { makeStyles } from '@material-ui/core/styles';
-
-import { colors } from '../../../config';
-
-export default makeStyles((theme) => ({
-
 import { topbarHeight } from '../../../config';
 import { colors } from '../../../config';
 
