a519d549bce7a13422aa3cf44965f7fe808ca100
Pravin <pravinkumar@vcollab.com>
2021-09-14 20:07:12 +0530

    modified build script

--- buildScripts/deployDemo.js
+++ buildScripts/deployDemo.js
@@ -8,12 +8,27 @@ if(fs.existsSync(dstFolder)){
 }
 
 fs.mkdirSync(dstFolder);
-
-fs.copyFile(srcFolder + '/index.html', dstFolder+'/viewer.html', (err) => {
+var ncp = require('ncp').ncp;
+  
+// Represents the number of pending
+// file system requests at a time.
+ncp.limit = 16;
+  
+// ncp(source, destination, callback) 
+ncp(srcFolder, dstFolder, 
+        function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Folders copied recursively');
+    fs.copyFile(srcFolder + '/index.html', dstFolder+'/viewer.html', (err) => {
+        if (err) throw err;
+        console.log(`${srcFolder} /index.html was copied to ${dstFolder} /viewer.html`);
+      });
+    fs.unlinkSync(dstFolder+'/index.html');
+    fs.copyFile(templateFolder + '/index.html', dstFolder+'/index.html', (err) => {
     if (err) throw err;
-    console.log(`${srcFolder} /index.html was copied to ${dstFolder} /viewer.html`);
-  });
-fs.copyFile(templateFolder + '/index.html', dstFolder+'/index.html', (err) => {
-if (err) throw err;
-console.log(`${templateFolder} /index.html was copied to ${dstFolder} /index.html`);
+    console.log(`${templateFolder} /index.html was copied to ${dstFolder} /index.html`);
+    });
 });
+
