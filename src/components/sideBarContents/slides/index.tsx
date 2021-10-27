9d48295cfa33c623b00822aff75f084afb64b77d
varunaravindvcollab <varunaravind@vcollab.com>
2021-10-27 22:55:18 +0530

    Slides body ui done.

--- src/components/sideBarContents/slides/index.tsx
+++ src/components/sideBarContents/slides/index.tsx
@@ -1,19 +1,63 @@
-import SideBarContainer from '../../layout/sideBar/sideBarContainer';
-
-import {useEffect} from 'react';
-
-import {goBack} from 'connected-react-router/immutable';
+import MuiIconButton from '@material-ui/core/IconButton';
 import Title from '../../layout/sideBar/sideBarContainer/sideBarHeader/utilComponents/Title';
 
-import {useAppSelector,useAppDispatch } from '../../../store/storeHooks';
+import SideBarContainer from '../../layout/sideBar/sideBarContainer';
+import BackButton from '../../icons/back';
 
-import BackButton from '../../../components/icons/back';
+import {useAppDispatch, useAppSelector} from '../../../store/storeHooks';
 
+import {goBack,push} from 'connected-react-router/immutable';
 
-import MuiIconButton from '@material-ui/core/IconButton';
+import RTree from '../../shared/RsTreeTable';
+ 
+import AddIcon from "@material-ui/icons/Add";
+
+import { selectSlideData, selectRootIds,expandNode  } from '../../../store/sideBar/slideSlice';
+
+import TreeNodeWithoutCheckbox from '../../shared/RsTreeTable/treeNodeWithoutCheckbox';
+import TreeCollapseIcon from '@material-ui/icons/ChevronRight';
+import TreeExpandedIcon from '@material-ui/icons/ExpandMore';
+import MuiGrid from '@material-ui/core/Grid';
+
+import { convertListToTree } from '../../utils/tree';
+
+import { useRef , useEffect} from 'react';
+import useContainer from '../../../customHooks/useContainer';
+
+import MuiEditIcon from '@material-ui/icons/EditOutlined';
+import MuiFileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import MuiPaste from '@material-ui/icons/AssignmentOutlined';
+import MuiDeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import MuiVisibilityIcon from '@material-ui/icons/Visibility';
+import OptionContainer from '../../layout/sideBar/sideBarContainer/sideBarFooter/utilComponents/OptionContainer'
+import Option from '../../layout/sideBar/sideBarContainer/sideBarFooter/utilComponents/Option'
+import MuiButton from '@material-ui/core/Button';
+import MuiTypography from '@material-ui/core/Typography';
+import MuiCheckIcon from '@material-ui/icons/Check';
+
+import MuiDownloadIcon from "@material-ui/icons/CloudDownloadOutlined";
+import MuicloudDoneIcon from '@material-ui/icons/CloudDone';
+
+import {Routes} from "../../../routes"
+
+import { useState } from 'react';
 
 
 export default function Slides (){
+
+    const treeDataRedux = useAppSelector(selectSlideData);
+  const treeRootIds = useAppSelector(selectRootIds);
+  const {roots, expanded} = convertListToTree(treeDataRedux,treeRootIds);
+
+  const containerRef = useRef(null);
+  const [containerWidth, containerHeight] = useContainer(containerRef,[treeDataRedux]);
+
+//   const selectedColorMapId = useAppSelector(state => state.colormap.selectedColorMapId);
+
+//   const appliedColorMapId = useAppSelector(state => state.colormap.appliedColorMapId);
+
+  const [openDelete, setOpenDelete] = useState(false);
+  const [copied, setCopied] = useState<any>();
 
     const dispatch = useAppDispatch();
 
@@ -21,8 +65,9 @@ export default function Slides (){
         dispatch(goBack());
     }
 
-  
-    
+    const handleExpand = (toOpen:boolean,nodeId:string) => {
+        dispatch(expandNode({toOpen,nodeId}));
+      }    
     
 
     const getHeaderLeftIcon = () => {
@@ -45,8 +90,86 @@ export default function Slides (){
 
     const getBody = () => {
         return (
-           <div>Coming Soon</div>
-        )
+            <div ref = {containerRef} style={{height:'100%',background:'transparent'}} >
+      <RTree 
+        treeData={roots} 
+        expandedRowIds = {expanded}
+        onExpand={handleExpand}
+        // onRowClick = {handleRowClick}
+        onRowClick = {() => null}
+        width = {300}
+        hover={true}
+        selectable={true}
+        // selected={[selectedColorMapId]}
+        selected={[]}
+        height = {containerHeight ? containerHeight - 5: 0}
+        renderTreeToggle = {
+          (icon,rowData) => {
+            if (rowData.children && rowData.children.length === 0) {
+              return null;
+            }
+            let state = treeDataRedux[rowData.id].state;
+            return state.expanded? <TreeExpandedIcon style={state.visibility ? {opacity:1.0} : {opacity:0.5}} viewBox="0 -7 24 24"/>:<TreeCollapseIcon style={state.visibility ? {opacity:1.0} : {opacity:0.5}} viewBox="0 -7 24 24"/>
+          }
+        }
+        treeNode = {(node) => {
+          return (
+            <TreeNodeWithoutCheckbox 
+              node={treeDataRedux[node.id]}
+              onCheck={() => console.log("sa")}
+            >
+            </TreeNodeWithoutCheckbox>
+          )
+        }}
+        column1 = {(node) => {
+          return (
+            <div></div>
+          )
+        }}
+        column2 = {(node) => {
+          return (
+            <div>
+              { node?.pid !== "-1" && node?.children.length === 0
+                ?
+                <MuiGrid container alignItems='center' style={{width:'100%',height:'100%'}}>
+                    <MuiGrid item xs={9}></MuiGrid>
+                    <MuiGrid item xs={3}>
+                { false
+              //   appliedColorMapId === node.id
+                  ?
+                      <MuiCheckIcon fontSize='small'/>
+                  :
+                    treeDataRedux[node.id].pid !== "-1"
+                      ?
+                        treeDataRedux[node.id].downloaded === true 
+                          ?
+                            <MuicloudDoneIcon fontSize='small'/>
+                          :
+                            <MuiDownloadIcon fontSize='small'/>
+                      :
+                        null
+                }
+                </MuiGrid>
+              </MuiGrid>
+                :        
+                  <MuiGrid container alignItems='center' style={{width:'100%',height:'100%'}}>
+                    <MuiGrid item xs={4}></MuiGrid>
+                    <MuiGrid item xs={6}>
+                      <MuiIconButton size='small' 
+                    //   onClick={() => handleCreateLabel(node.id)}
+                      >
+                        <AddIcon fontSize='default'/> 
+                      </MuiIconButton> 
+                    </MuiGrid>
+                  </MuiGrid>
+              }    
+            </div>
+          )
+        }}
+      />  
+    </div>
+    )
+        
     }
 
     const getFooter = () => {
--- src/store/sideBar/slideSlice.ts
+++ src/store/sideBar/slideSlice.ts
@@ -4,32 +4,209 @@ import {TreeNode, ITreeState} from "./shared/ProductExplorer/types";
 import {saveTreeReducer, checkNodeReducer, highlightNodeReducer, invertNodeReducer, expandNodeReducer, toggleVisibilityReducer, setCheckedVisibilityReducer} from "./shared/ProductExplorer/reducers";
 // Define a type for the slice state
 
-interface ProductTreeState extends ITreeState {
-    data: {[id:string]:TreeNode},
+interface SlideTreeNode extends TreeNode {
+    downloaded : boolean,
+}
+
+interface SlideTreeState extends ITreeState {
+    data: {[id:string]:SlideTreeNode},
     rootIds: string[],
 }
 
 // Define the initial state using that type
-const initialState: ProductTreeState = {
+const initialState: SlideTreeState = {
     data: {
         "0" : {
             id : "0",
                 pid : "-1",
                 title: "Presentation 1",
-                children: ["2","3"],
+                children: ["01","02","03","04","05","06"],
                 state: {
                     expanded: true,
                     visibility: true,
                 },
+                downloaded: false,
                 attributes: {},
         },
+
+      "01" : {
+            id : "01",
+                pid : "0",
+                title: "Stress Animation",
+                children: ["011","012","013","014","015"],
+                state: {
+                    expanded: true,
+                    visibility: true,
+                },
+                downloaded: false,
+                attributes: {},
+        },
+
+        "011" : {
+          id : "011",
+              pid : "01",
+              title: "Step 1",
+              children: [],
+              state: {
+                  expanded: true,
+                  visibility: true,
+              },
+              downloaded:false,
+              attributes: {},
+      },
+
+      "012" : {
+        id : "012",
+            pid : "01",
+            title: "Step 2",
+            children: [],
+            state: {
+                expanded: true,
+                visibility: true,
+            },
+            downloaded: true,
+            attributes: {},
     },
-    rootIds: [],
+
+    "013" : {
+      id : "013",
+          pid : "01",
+          title: "Step 3",
+          children: [],
+          state: {
+              expanded: true,
+              visibility: true,
+          },
+          downloaded: false,
+          attributes: {},
+  },
+
+  "014" : {
+    id : "014",
+        pid : "01",
+        title: "Step 4",
+        children: [],
+        state: {
+            expanded: true,
+            visibility: true,
+        },
+        downloaded:false,
+        attributes: {},
+},
+
+"015" : {
+  id : "015",
+      pid : "01",
+      title: "Step 5",
+      children: [],
+      state: {
+          expanded: true,
+          visibility: true,
+      },
+      downloaded: true,
+      attributes: {},
+},
+
+
+
+
+        "02" : {
+          id : "02",
+              pid : "0",
+              title: "Reaction Force",
+              children: [],
+              state: {
+                  expanded: true,
+                  visibility: true,
+              },
+              downloaded: true,
+              attributes: {},
+      },
+
+      "03" : {
+        id : "03",
+            pid : "0",
+            title: "Applied Loads",
+            children: [],
+            state: {
+                expanded: true,
+                visibility: true,
+            },
+            downloaded: false,
+            attributes: {},
+    },
+
+    "04" : {
+      id : "04",
+          pid : "0",
+          title: "Displacement",
+          children: [],
+          state: {
+              expanded: true,
+              visibility: true,
+          },
+          downloaded:true,
+          attributes: {},
+  },
+
+  "05" : {
+    id : "05",
+        pid : "0",
+        title: "View 1",
+        children: [],
+        state: {
+            expanded: true,
+            visibility: true,
+        },
+        downloaded: true,
+        attributes: {},
+},
+
+"06" : {
+  id : "06",
+      pid : "0",
+      title: "View 2",
+      children: [],
+      state: {
+          expanded: true,
+          visibility: true,
+      },
+      downloaded: false,
+      attributes: {},
+},
+
+        "1" : {
+          id : "1",
+              pid : "-1",
+              title: "Group 1",
+              children: [],
+              state: {
+                  expanded: true,
+                  visibility: true,
+              },
+              downloaded: false,
+              attributes: {},
+      },
+
+      "2" : {
+        id : "2",
+            pid : "-1",
+            title: "Group 2",
+            children: [],
+            state: {
+                expanded: true,
+                visibility: true,
+            },
+            downloaded:false,
+            attributes: {},
+    },
+    },
+    rootIds: ["0","1","2"],
 }
 
 
-export const productTreeSlice = createSlice({
-  name: 'productTree',
+export const slideSlice = createSlice({
+  name: 'slide',
   initialState,
   reducers: {
     saveTree: saveTreeReducer,
@@ -49,23 +226,10 @@ export const {
   checkNode, 
   invertNode, 
   expandNode,
-   } = productTreeSlice.actions;
+   } = slideSlice.actions;
 
 //Define the selectors
-export const selectProductTreeData = (state:RootState) => state.productTree.data
-export const selectModels = (state:RootState) => state.productTree.rootIds.map((id) => state.productTree.data[id]);
-export const selectCurrentState = (state:RootState) => state.productTree.currentState
-export const selectRootIds = (state:RootState) => state.productTree.rootIds
-export const selectSearchHints = (state:RootState) => Object.keys(state.productTree.searchHints)
-export const selectPrevSearches = (state:RootState) => Object.keys(state.productTree.prevSearches)
-export const selectSearchString = (state:RootState) => state.productTree.searchQuery
-export const selectSearchResults = (state:RootState) => state.productTree.searchResults
-export const selectCheckedLeafNodes = (state:RootState):TreeNode[] => {
-  let nodes = [...Object.values(state.productTree.data)] as TreeNode[];
-  return nodes.filter((item: TreeNode) => item.children.length === 0 && item.state.checked);
-}
-export const selectUnCheckedLeafNodes = (state:RootState):TreeNode[] => {
-  let nodes = [...Object.values(state.productTree.data)] as TreeNode[];
-  return nodes.filter((item: TreeNode) => item.children.length === 0 && item.state.checked === false);
-}
-export default productTreeSlice.reducer;
+export const selectSlideData = (state:RootState) => state.slide.data
+export const selectRootIds = (state:RootState) => state.slide.rootIds
+
+export default slideSlice.reducer;
